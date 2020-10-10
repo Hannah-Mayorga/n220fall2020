d3db66c6b62ae7673c5e2b4a9e1b785e91589f02
Hannah Mayorga <40367679+Hannah-Mayorga@users.noreply.github.com>
2020-10-10 00:35:16 -0400

    animation pt 2

--- labs/ui-animation/js/app.js
+++ labs/ui-animation/js/app.js
@@ -1,4 +1,42 @@
-let elementsDiv = document.getElementById("elements");
-let x = 400;
-console.log(x);
-gsap.to(elementsDiv, {duration: 2, x: 100});
+let elementsDiv = document.getElementsByClassName("elements");
+let blueDiv = document.getElementById("blueBox");
+let purpleDivs = document.getElementsByClassName("purpleBox");
+let purple = document.getElementById("purple");
+for (let i = 0; i < elementsDiv.length; i++) {
+    TweenLite.from(elementsDiv[i], {
+        duration: 1 * .4,
+        x: -100,
+        alpha: 0
+    });
+}
+blueDiv.addEventListener("mouseover", onTrailClick);
+
+function onTrailClick() {
+    TweenMax.to(blueDiv, {
+        duration: 4,
+        x: 200
+    });
+}
+blueDiv.addEventListener("click", clickBack);
+
+function clickBack() {
+    TweenMax.to(blueDiv, {
+        duration: 4,
+        x: -200
+    });
+    TweenMax.to(purpleDivs, {
+        duration: 3,
+        x: -750,
+
+    });
+}
+purple.addEventListener("mouseover", GroupClick);
+
+function GroupClick() {
+    TweenMax.to(purpleDivs, {
+        duration: 3,
+        x: 750,
+        rotation: 360
+
+    });
+}
